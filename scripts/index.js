4d27dc05ebd2b21c750c26bb9ebc4ca5385c5602
nvhiii <nahikhan16@gmail.com>
2024-03-07 20:28:13 -0500

    added script styling for grid + div

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+const gridContainer = document.querySelector(`.grid-container`);
+const grid = document.createElement(`div`);
+
+// 16x16 grid
+grid.style.display = grid;
+grid.style.gridTemplateColumns =
+  "auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto";
+grid.style.gridTemplateRows =
+  "auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto";
